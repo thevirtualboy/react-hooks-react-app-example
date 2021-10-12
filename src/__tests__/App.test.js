b327bb3739028dc0d9e2b91f75f297933e31e560
Peter Fontana <dude0508@gmail.com>
2021-10-12 11:09:03 -0400

    commit tree 2827f947ae2aa46b2b7fa24af5513d21276e17b3

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,4 +1,5 @@
 import "@testing-library/jest-dom";
+import React from 'react';
 import { render, screen } from "@testing-library/react";
 import { format } from "date-fns";
 import App from "../components/App";
